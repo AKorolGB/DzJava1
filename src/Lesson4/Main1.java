7f15d71c17aa24752009f844800789a596bf4041
AKorolGB <Alexander94.13@mail.ru>
2023-07-14 16:34:50 +0200

    Zad2

---
+++ src/Lesson4/Main1.java
@@ -0,0 +1,46 @@
+package Lesson4;
+//Найдите сумму всех элементов LinkedList, сохраняя все элементы в списке. Используйте итератор
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class Main1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = generateRandomNambers(10,1,10);
+        System.out.println(numbers);
+        System.out.println(calculateSum(numbers));
+        System.out.println(calculateSumiterator(numbers));
+    }
+
+    public static List<Integer> generateRandomNambers (int count,int min, int max) {
+        List<Integer> numbers = new LinkedList<>();
+        Random random = new Random();
+
+        for (int i = 1; i < count; i++){
+            int randomNumber = random.nextInt(max - min + 1) + min;
+            numbers.add(randomNumber);
+        }
+        return numbers;
+    }
+
+    public static int calculateSum(List<Integer> numbers){
+        int sum = 0;
+                for (Integer number: numbers){
+                 sum = sum + number;
+            }
+        return sum;
+    }
+
+    public static int calculateSumiterator(List<Integer> numbers) {
+        int sum = 0;
+        Iterator<Integer> iterator = numbers.iterator();
+
+        while (iterator.hasNext()) {
+            int element = iterator.next();
+            sum += element;
+        }
+        return sum;
+    }
+}
